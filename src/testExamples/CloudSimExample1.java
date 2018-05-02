abca91284de2bf8f2d29d62135d54b4bfb2f293a
AbderrahmanL <lahiaouni.abd@gmail.com>
2018-05-02 17:04:43 +0200

    first commit

---
+++ src/testExamples/CloudSimExample1.java
@@ -0,0 +1,17 @@
+package testExamples;
+
+import java.util.List;
+
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.Vm;
+
+public class CloudSimExample1 {
+	private static List<Cloudlet> cloudletList;
+	private static List<Vm> vmList;
+	
+	public static void main(String[] args) {
+			
+	}
+	
+	
+}
